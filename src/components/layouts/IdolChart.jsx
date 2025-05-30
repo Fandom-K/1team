6c41692991d8ba057c6072e6b92d965ff1d6d212
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-05-30 21:03:27 +0900

    [배민지] feat: IdolChart 추가

---
+++ src/components/layouts/IdolChart.jsx
@@ -0,0 +1,11 @@
+const IdolChart = () => {
+  return (
+    <div>
+      <title>이달의 차트</title>
+      <Button>이달의 여자 아이돌</Button>
+      <Button>이달의 여자 아이돌</Button>
+    </div>
+  );
+};
+
+export default IdolChart;
