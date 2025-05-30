9c47e11b653e41399a89e99b6875b78c000665f8
jirene0 <999gin@gmail.com>
2025-05-30 20:46:21 +0900

    [상진규] feat:버튼 컴포넌트 추가

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,8 @@
+import Button from "../components/common/Button";
+
 const LandingPage = () => {
+  const CheckCredit = true;
+
   return (
     <div className="LandingPage">
       <section>LandingPage</section>
