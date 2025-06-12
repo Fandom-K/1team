8f33c1806a63e49d326f994ada84fec469706c50
mingyeong <primg67@naver.com>
2025-06-12 16:43:06 +0900

    [김민경] 오류 수정

--- src/services/getIdols.jsx
+++ src/services/getIdols.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getIdols() {
   const response = await axios.get(
-    `https://fandom-k-api.vercel.app/16-1/idols/?pageSize=100`
+    `https://fandom-k-api.vercel.app/16-1/idols?pageSize=100`
   );
   return response.data.list;
 }
