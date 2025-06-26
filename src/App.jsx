9e9a6ea37cc77704af45e02d273516fa1cb0836c
김이서 <12305025k@gmail.com>
2025-06-26 10:30:31 +0900

    마이페이지 리팩토링

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import ListPage from "./pages/ListPage";
-import ExamplePage from "./pages/ExamplePage";
 import MyPage2 from "./pages/MyPage2";
-import MyPage from "./pages/Mypage";
 
 function App() {
   return (
